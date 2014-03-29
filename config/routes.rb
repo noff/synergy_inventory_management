8642b2fe98b76ace471864ed6c1c3ec324dc6295
Michael Kechinov <sam@mkechinov.ru>
2014-03-29 11:58:50 +0400

    Removed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
-Spree::Core::Engine.routes.draw do
-  #namespace :admin do
-  #  match 'edit_multiple_products' => "products#edit_multiple", :as => :edit_multiple_products_start, :via => :get
-  #  match 'edit_multiple_products/:id' => "products#edit_multiple", :as => :edit_multiple_products, :via => :get
-  #  match 'edit_multiple_products/:id' => "products#update_multiple", :via => :put
-  #  match 'edit_multiple_products/:id' => "products#destroy_multiple", :via => :delete
-  #  match 'switch_product_availability/:id' => "products#switch_availability", :via => :put, :as => :switch_product_availability
-  #end
-end
+#Spree::Core::Engine.routes.draw do
+#  #namespace :admin do
+#  #  match 'edit_multiple_products' => "products#edit_multiple", :as => :edit_multiple_products_start, :via => :get
+#  #  match 'edit_multiple_products/:id' => "products#edit_multiple", :as => :edit_multiple_products, :via => :get
+#  #  match 'edit_multiple_products/:id' => "products#update_multiple", :via => :put
+#  #  match 'edit_multiple_products/:id' => "products#destroy_multiple", :via => :delete
+#  #  match 'switch_product_availability/:id' => "products#switch_availability", :via => :put, :as => :switch_product_availability
+#  #end
+#end
